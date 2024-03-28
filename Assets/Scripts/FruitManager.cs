3f44ebfa5f5913a4ee01dba9bb77dd42f999839e
Viet181099 <64130233+Viet181099@users.noreply.github.com>
2024-03-28 21:24:35 +0700

    Day1

---
+++ Assets/Scripts/FruitManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+
+    [Header("Elements")]
+    [SerializeField] GameObject fruitPrefabs;
+    [Header("Settings")]
+    [SerializeField] float fruitYPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ManagerPlayerInput();
+        }
+    }
+
+    void ManagerPlayerInput()
+    {
+        // đổi vị trí click từ trên xuống
+        Vector2 pos = GetClickedWorkPosition();
+        pos.y = fruitYPosition;
+        Instantiate(fruitPrefabs, pos, Quaternion.identity);
+
+        // Spawn prefabs tại điểm click chuột9
+        //Instantiate(fruitPrefabs, GetClickedWorkPosition(), Quaternion.identity);
+    }
+
+    // click chuột lấy toạ độ trên Screen
+    public Vector2 GetClickedWorkPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
